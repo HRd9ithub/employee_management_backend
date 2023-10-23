33e05f32c411b3b465ddc51b29c19526d1f0db90
hardik <hardik.d9ithub@gmail.com>
2023-10-23 10:09:17 +0530

    error solve

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async() => {
     try {
         await mongoose.connect(process.env.URL, {useNewUrlParser: true,useUnifiedTopology: true});
-        console.log("Database connect successfully.")
+        console.log(">>>>>>>>>>>>  Database connect successfully.")
 
     } catch (error) {
         console.log('error ======================= >>>>>>>>>>>>', error)
